20e2749902f9073ed575d886a1b5b99c96ad9287
Anurima-mouli <63995474+Anurima-mouli@users.noreply.github.com>
2022-09-21 12:43:35 +0530

    Update findJane.sh
    findJane.sh is running ok but not returning the full path of the files. this missing path is causing the second changeJane.py fail. $HOME is automatically taking the file location from the user specific location. No need to put the exact location manually.

--- findJane.sh
+++ findJane.sh
@@ -2,6 +2,6 @@
 grep " jane " ../data/list.txt | cut -d ' ' -f 3 > tempFile.txt
 files=$(<tempFile.txt)
 for i in $files ; do
- if test -e "..$i"; then echo "$i" >> oldFiles.txt;
+ if test -e "$HOME$i"; then echo "$HOME$i" >> oldFiles.txt;
  fi
 done
